1cd3baa9c0844236ce7db0292f4a44e9900d4825
Pranav Nair <pranavnswa@gmail.com>
2022-10-31 21:44:34 -0700

    updated with tab controller

---
+++ iOS_Spotify/iOS_Spotify/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  iOS_Spotify
+//
+//  Created by Amy Wang on 10/24/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func onSignIn(_ sender: Any) {
+        
+    }
+    
+    
+
+}
+
